4b5a04064cbcc75242b79a6575af5eb1602c1993
Herbert Poul <h.poul@tapo.at>
2014-01-20 22:32:02 +0100

    load json from dart instead of javascript.

--- lib/tapo_graph.dart
+++ lib/tapo_graph.dart
@@ -2,18 +2,29 @@ library tapo_graph;
 
 import 'dart:js';
 import 'package:polymer/polymer.dart';
+import 'dart:html';
+import 'dart:convert';
 
 @CustomTag('tapo-graph')
 class TapoGraph extends PolymerElement {
   TapoGraph.created() : super.created();
   
   @published String prefix;
+  @published String loadjson;
   
   void enteredView() {
     super.enteredView();
+    
+    HttpRequest.getString(loadjson).then((v) {
+      var obj = JSON.decode(v);
+      context.callMethod('loadAndDraw', 
+          [ getShadowRoot('tapo-graph').querySelector('.chartwrapper'),
+            prefix,
+            new JsObject.jsify(obj)]);
+    });
+    
     //context.callMethod('blubber');
     //context.callMethod('blubber', [getShadowRoot('tapo-graph').querySelector('.testing')]);
-    context.callMethod('loadAndDraw', [getShadowRoot('tapo-graph').querySelector('.chartwrapper'), prefix]);
 //    context['blubber'];
   }
 }
